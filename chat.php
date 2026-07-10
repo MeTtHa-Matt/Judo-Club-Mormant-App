fb3f5552cec2d76360edd09445d818b8ca62945e
MeTtHa-Matt <24mgimenez@gmail.com>
2026-07-10 22:40:58 +0200

    oublie

--- chat.php
+++ chat.php
@@ -129,6 +129,9 @@ if (!$me || (int) $me['admin'] !== 1) {
     </main>
 
     <script src="js/chat.js?v=<?php echo filemtime('js/chat.js'); ?>" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
